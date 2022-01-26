e31bcaca4f273f242ff4fedcfca07510b53c1ebc
lex <rlexgarcia88@gmail.com>
2022-01-26 10:58:04 +0800

    directory changes

--- cp.py
+++ cp.py
@@ -1,7 +1,7 @@
 #below 3 lines must be modified as needed
 file_filter = '*.dwg'
-source_dirs = 'D:\10_Shigoto\01_Tsuneishi Technical Services (Phils.), Inc\D10Afra\01_KEY PLAN\02_DRAWINGS\110916_DATA\000520 BODY;D:\10_Shigoto\01_Tsuneishi Technical Services (Phils.), Inc\D10Afra\01_KEY PLAN\02_DRAWINGS\110916_DATA\000510 SHELL'
-destination_dir = 'D:\00_Continuous Professional Development\Udemy\Pyhton\Test Destination'
+source_dirs = 'D:\Test\000520 BODY;D:\Test\000510 SHELL'
+destination_dir = 'D:\Test\Destination'
 
 #actual script to copy the files
 from pathlib import Path
