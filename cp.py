4a95f4939a93500edcee4ea7093e68897d98c749
Nash <arbignay@yahoo.com>
2022-01-28 11:01:17 +0800

    allow copy of multiple source and destination directories via a mapping file

--- cp.py
+++ cp.py
@@ -1,26 +1,40 @@
-#below 3 lines must be modified as needed
-file_filter = '*.dwg'
-source_dirs = 'D:/10_Shigoto/05_Asia Projects Engineering Pte Ltd (APECO)/C1805 - PIPERACK/'
-destination_dir = 'D:/Test/Destination/'
-
-#actual script to copy the files
 from pathlib import Path
 import shutil
 import os
+import sys
 
-print('Copying files...')
-rn = 0
-for sd in source_dirs.split(';'):
-    for d in Path(sd).rglob(file_filter):
-        #print(d)
+if len(sys.argv) != 2:
+    print('Invalid program arguments!!!')
+    sys.exit(1)
+
+file_filter = (sys.argv[1])
+configs = open('config.txt', 'r').readlines()
+
+print('Validating config.txt contents...')
+for c in configs:
+    ok = True
+    sd, dd = c.split(';')
+    if not sd.strip().endswith('/') or not dd.strip().endswith('/'):
+        ok = False
+    if '\\' in sd.strip() and '\\' in dd.strip():
+        ok = False
+    if not ok:
+        print('Invalid content of config.txt file, please validate')
+        sys.exit(1)
+
+print('Copying files with file extraction filter as : ' + file_filter)
+for c in configs:
+    rn = 0
+    sd, dd = c.split(';')
+    print('Copying files from ' + sd + ' to ' + dd.strip() + '...')
+    for d in Path(sd.strip()).rglob(file_filter):
         base, fn = os.path.split(d)
-        if not os.path.isfile(destination_dir + fn):
-            shutil.copyfile(d, destination_dir + fn)
-            print('copied as : ' + str(d))
+        if not os.path.isfile(dd.strip() + fn):
+            shutil.copyfile(d, dd.strip() + fn)
         else:
             rn = rn + 1
             fn, ext = os.path.splitext(fn)
             fn = fn + '-duplicated-' + str(rn) + ext
-            shutil.copyfile(d, destination_dir + fn)
-            print('copied as : ' + base + '\\' + fn)
+            shutil.copyfile(d, dd.strip() + fn)
 print('Done')
+sys.exit(0)
